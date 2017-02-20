20ae886f0863702bb9cd209926adef5622e54ac2
hukurou-s <s14t206@stmail.eng.kagawa-u.ac.jp>
2017-02-21 05:34:57 +0900

    論文情報の登録機能を実装

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -7,4 +7,32 @@ class PapersController < ApplicationController
   def show
     @paper = Paper.find(params[:id])
   end
+
+  def new
+    @paper = Paper.new
+  end
+
+  def create
+    paper = params['paper']
+    Paper.create(
+      title_J: paper['title_J'],
+      title_E: paper['title_E'],
+      Lead_author: paper['Lead_author'],
+      Co_author: paper['Co_author'],
+      abstract: paper['abstract']
+    )
+    redirect_to papers_path
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
